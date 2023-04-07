3595146dc79e4642ee57d8817a25b30e33c066a8
a-sumo <armandsumo@gmail.com>
2023-04-08 00:15:05 +0200

    add lighting_model to material struct

--- src/renderer/material/volume_raycasting_material.rs
+++ src/renderer/material/volume_raycasting_material.rs
@@ -11,20 +11,26 @@ pub struct VolumeRaycastingMaterial {
     pub voxels: std::sync::Arc<Texture3D>,
     /// The size of the cube that is used to render the voxel data. The texture is scaled to fill the entire cube.
     pub size: Vec3,
+    /// The lighting model used when rendering this material
+    pub lighting_model: LightingModel,
 }
 
 impl Material for VolumeRaycastingMaterial {
-    fn fragment_shader(&self) -> FragmentShader {
+    fn fragment_shader(&self, lights: &[&dyn Light]) -> FragmentShader {
+        let mut source = lights_shader_source(lights, self.lighting_model);
+        source.push_str(include_str!("shaders/volume_raycasting_material.frag"));
         FragmentShader {
-            source: include_str!("shaders/volume_raycasting_material.frag").to_string(),
+            source,
             attributes: FragmentAttributes {
                 position: true,
                 ..FragmentAttributes::NONE
             },
         }
     }
-
-    fn use_uniforms(&self, program: &Program, camera: &Camera) {
+    fn use_uniforms(&self, program: &Program, camera: &Camera, lights: &[&dyn Light]) {
+        for (i, light) in lights.iter().enumerate() {
+            light.use_uniforms(program, i as u32);
+        }
         program.use_uniform("cameraPosition", camera.position());
         program.use_uniform("size", self.size);
         program.use_texture_3d("tex", &self.voxels);
@@ -47,6 +53,7 @@ impl FromCpuVoxelGrid for VolumeRaycastingMaterial {
         Self {
             voxels: std::sync::Arc::new(Texture3D::new(context, &cpu_voxel_grid.voxels)),
             size: cpu_voxel_grid.size,
+            lighting_model: LightingModel::Blinn,
         }
     }
 }
